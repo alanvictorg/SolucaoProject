21ba8341be1e8bacbbbe5d23e8b3073679159aac
Thiago Dionizio <thiago.o.dionizio@gmail.com>
2017-05-12 15:33:08 -0300

    Import

--- app/Http/Controllers/Admin/ImportsController.php
+++ app/Http/Controllers/Admin/ImportsController.php
@@ -343,6 +343,7 @@ class ImportsController extends Controller
         echo 'display_errors = ' . ini_get('display_errors') . "\n";
         echo 'register_globals = ' . ini_get('register_globals') . "\n";
         echo 'post_max_size = ' . ini_get('post_max_size') . "\n";
+        echo 'memory_limit = ' . ini_get('memory_limit') . "\n";
         echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "\n";
 
 //        $checkDom = new \DOMDocument('1.0', 'UTF-8');
